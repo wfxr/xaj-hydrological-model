f24d18bdf7f9bc86f13952a8ed1e8b3bdbf30977
wfxr <wfxrgm@gmail.com>
2016-01-28 00:47:48 +0800

    Add javadoc

--- src/HydrologicalModelHelper/ModelEvaluation.java
+++ src/HydrologicalModelHelper/ModelEvaluation.java
@@ -5,10 +5,19 @@ import java.util.stream.DoubleStream;
 import static HydrologicalModelHelper.ParameterValidation.*;
 
 /**
+ * 模型评价工具类
+ *
  * Created by Wenxuan on 1/27/2016.
  * Email: wenxuan-zhang@outlook.com
  */
 public class ModelEvaluation {
+    /**
+     * 计算模型预测结果的纳什效率系数
+     *
+     * @param obs 实际观测值序列
+     * @param pre 模型预测值序列
+     * @return 纳什效率系数
+     */
     public static double NashSutcliffeEfficiency(double[] obs, double[] pre) {
         int n = CheckIdenticalLength(obs, pre);
         double avg = DoubleStream.of(obs).average().getAsDouble();
--- src/HydrologicalModelHelper/ParameterValidation.java
+++ src/HydrologicalModelHelper/ParameterValidation.java
@@ -3,10 +3,18 @@ package HydrologicalModelHelper;
 import static com.google.common.base.Preconditions.*;
 
 /**
+ * 参数验证工具类
+ *
  * Created by Wenxuan on 1/27/2016.
  * Email: wenxuan-zhang@outlook.com
  */
 public class ParameterValidation {
+    /**
+     * 确保所有的待检验的数组长度一致
+     *
+     * @param arrays 待检验的数组
+     * @return 如果所有数组长度一致则返回数组的长度
+     */
     public static int CheckIdenticalLength(double[]... arrays) {
         checkNotNull(arrays);
         checkElementIndex(0, arrays.length);
